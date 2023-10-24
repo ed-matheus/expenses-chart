1ccf234616acaac290dd6a199da014301b6587b6
ed-matheus <edsonmatheus02@hotmail.com>
2023-10-24 00:32:06 -0300

    general changes and progress with JS code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,24 @@
+// Selecting Elements
+const chartBars = document.querySelectorAll('.chart div')
+
+// Calling the JSON file
 fetch("../data.json")
     .then(response => response.json())
     .then(data => {
         console.log(data)
 
-
+        chartBars.forEach(div => {
+            console.log(div)
+            // div.addEventListener('mouseover', hiddenData())
+        });
     })
-    .catch(a => {
-        console.error("Erro ao carregar o arquivo JSON: ", a)
+    .catch(error => {
+        console.error("Couldn't reload the JSON file: ", error)
     });
+
+// "hover" function
+/*
+function hiddenData() {
+
+}
+*/
