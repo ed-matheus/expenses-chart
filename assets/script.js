ef28e080fbab8b44f04cf3245265040bf8e417b3
ed-matheus <edsonmatheus02@hotmail.com>
2023-10-24 16:32:55 -0300

    data requisition and JS code finished

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // Selecting Elements
+const popUp = document.querySelector('.popup')
 const chartBars = document.querySelectorAll('.chart div')
+const chartLabels = document.querySelectorAll('.labels span') 
 
 // Calling the JSON file
 fetch("../data.json")
@@ -7,18 +9,37 @@ fetch("../data.json")
     .then(data => {
         console.log(data)
 
-        chartBars.forEach(div => {
-            console.log(div)
-            // div.addEventListener('mouseover', hiddenData())
+        chartBars.forEach((bar, index) => {
+            bar.addEventListener('mouseover', () => {
+                showPopup(bar, data[index].amount);
+            })
+
+            bar.addEventListener('mouseout', () => {
+                hidePopup();
+            })
         });
+
+        chartLabels.forEach((label, index) => {
+            const day = data[index].day
+            label.textContent = day
+        })
     })
     .catch(error => {
         console.error("Couldn't reload the JSON file: ", error)
     });
 
 // "hover" function
-/*
-function hiddenData() {
+function showPopup(e, amount) {
+    const popup = document.getElementById('popup');
+    const barraRect = e.getBoundingClientRect();
 
+    popup.style.display = 'block';
+    popup.style.top = barraRect.top - 40 + 'px';
+    popup.style.left = barraRect.left + 'px';
+    popup.textContent = `$${amount}`;
 }
-*/
+
+function hidePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
